c3fc679c738b67e993a092bd66f0690f246254b5
Felipe Daniel <felipe.daniel@bememergencias.com.br>
2022-02-27 03:28:56 -0300

    Components medium level

--- iniciando/script.js
+++ iniciando/script.js
@@ -9,18 +9,11 @@ Vue.component('my-app', {
         <titulo></titulo>
         <div class="row">
             <div class="col-md-12">
-                <new-play :teams="teams" @new-play="showScore($event)"></new-play>
+                <new-play :teams="teams"></new-play>
             </div>            
         </div>
     
         <div class="row">            
-            <div class="col-md-12" v-if="view!='table'">    
-                <score 
-                    :house-team="houseTeam" 
-                    :out-team="outTeam"
-                    @end-play="view = 'table'"></score>
-            </div>    
-
             <div class="col-md-12" v-show="view === 'table'">
                 <table-teams :teams="teams"></table-teams>
             </div>
@@ -29,33 +22,35 @@ Vue.component('my-app', {
     `,
     data() {
         return {
-            teams: [
-                new Team('América MG', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/america_mg_60x60.png?raw=true'),
-                new Team('Atlético PR', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/atletico-pr_60x60.png?raw=true'),
-                new Team('Atlético MG', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/atletico_mg_60x60.png?raw=true'),
-                new Team('Bahia', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/bahia_60x60.png?raw=true'),
-                new Team('Botafogo', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/botafogo_60x60.png?raw=true'),
-                new Team('Ceará', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/ceara_60x60.png?raw=true'),
-                new Team('Chapecoense', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/chapecoense_60x60.png?raw=true'),
-                new Team('Corinthians', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/corinthians_60x60.png?raw=true'),
-                new Team('Cruzeiro', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/cruzeiro_60x60.png?raw=true'),
-                new Team('Flamengo', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/flamengo_60x60.png?raw=true'),
-                new Team('Fluminense', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/fluminense_60x60.png?raw=true'),
-                new Team('Gremio', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/gremio_60x60.png?raw=true'),
-                new Team('Internacional', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/internacional_60x60.png?raw=true'),
-                new Team('Palmeiras', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/palmeiras_60x60.png?raw=true'),
-                new Team('Paraná', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/parana_60x60.png?raw=true'),
-                new Team('Santos', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/santos_60x60.png?raw=true'),
-                new Team('São Paulo', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/sao_paulo_60x60.png?raw=true'),
-                new Team('Sport', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/sport_60x60.png?raw=true'),
-                new Team('Vasco', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/vasco_60x60.png?raw=true'),
-                new Team('Vitória', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/vitoria_60x60.png?raw=true')
-            ], 
+            // teams: [
+            //     new Team('América MG', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/america_mg_60x60.png?raw=true'),
+            //     new Team('Atlético PR', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/atletico-pr_60x60.png?raw=true'),
+            //     new Team('Atlético MG', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/atletico_mg_60x60.png?raw=true'),
+            //     new Team('Bahia', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/bahia_60x60.png?raw=true'),
+            //     new Team('Botafogo', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/botafogo_60x60.png?raw=true'),
+            //     new Team('Ceará', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/ceara_60x60.png?raw=true'),
+            //     new Team('Chapecoense', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/chapecoense_60x60.png?raw=true'),
+            //     new Team('Corinthians', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/corinthians_60x60.png?raw=true'),
+            //     new Team('Cruzeiro', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/cruzeiro_60x60.png?raw=true'),
+            //     new Team('Flamengo', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/flamengo_60x60.png?raw=true'),
+            //     new Team('Fluminense', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/fluminense_60x60.png?raw=true'),
+            //     new Team('Gremio', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/gremio_60x60.png?raw=true'),
+            //     new Team('Internacional', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/internacional_60x60.png?raw=true'),
+            //     new Team('Palmeiras', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/palmeiras_60x60.png?raw=true'),
+            //     new Team('Paraná', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/parana_60x60.png?raw=true'),
+            //     new Team('Santos', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/santos_60x60.png?raw=true'),
+            //     new Team('São Paulo', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/sao_paulo_60x60.png?raw=true'),
+            //     new Team('Sport', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/sport_60x60.png?raw=true'),
+            //     new Team('Vasco', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/vasco_60x60.png?raw=true'),
+            //     new Team('Vitória', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/vitoria_60x60.png?raw=true')
+            // ], 
             houseTeam: null,
             outTeam: null,
+            teams: this.teamsCollection,
             view: 'table' 
         };   
     },
+    inject: ['teamsCollection'],
     methods: {                
         endingPlay() {
             let byHouse = this.newPlay.byHouse;
@@ -88,29 +83,26 @@ Vue.component('dteam', {
     props : ['team','inverse'],
     template: `
     <div style="display:flex; flex-direction:row">
-    <img class="logo" :src="team.logo" :style="{order: inverse == 'true' ? 2 : 1}" @click="parentAlert">
+    <img class="logo" :src="team.logo" :style="{order: inverse == 'true' ? 2 : 1}">
     <span :style="{order: inverse == 'true' ? 1 : 2}">{{ team.name }}</span>
     </div>
-    `,
-    methods: {
-        parentAlert() {
-            alert(this.$parent.houseTeam.name + ' --> ' + this.$parent.outTeam.name)
-        }
-    }
+    `    
 });
 
 
 Vue.component('table-teams', {
-    props:['teams'],
+    // props:['teams'],
     data() {
         return {
             search: '',
             order: {
                 fields: ['points', 'gm', 'gs', 'balance'],
                 sort: ['desc', 'desc', 'desc', 'desc']
-            }            
+            },
+            teams: this.teamsCollection          
         }
     },
+    inject: ['teamsCollection'],
     template: `
         <div>
         
@@ -213,48 +205,24 @@ Vue.component('relegated-teams', {
     }
 })
 
-
-Vue.component('score', {
-    props: ['houseTeam', 'outTeam'],
-    data() {
-        return {
-            goalsByHouse: 0,
-            goalsByOut: 0
-        }
-    },
-    template: `
-    <div>
-        <form action="" class="form-inline">
-                
-        <input type="text" class="form-control col-md-1" v-model="goalsByHouse">                
-        <dteam :team="houseTeam" inverse="true"></dteam>
-
-        <span><h2>X</h2></span>
-
-        <dteam :team="outTeam"></dteam>                
-        <input type="text" class="form-control col-md-1" v-model="goalsByOut">
-
-        <button type="button" class="btn btn-primary" @click="endPlay">Fim de Jogo</button>            
-        </form>
-    </div>    
-    `,
-    methods: {
-        endPlay() {
-            var gm = parseInt(this.goalsByHouse); 
-            var gs = parseInt(this.goalsByOut);
-            this.houseTeam.endPlay(this.outTeam, gm, gs);
-            this.$emit('end-play');
-        }
-    }
-})
-
 Vue.component('new-play', {
     template: `
     <div>
-        <button class="btn btn-primary" @click="createNewPlay">Novo Jogo</button>
+        <button 
+            class="btn btn-primary" 
+            @click="createNewPlay">Novo Jogo</button>
+        <score-modal :houseTeam="houseTeam" :outTeam="outTeam" ref="modal"></score-modal>
     </div>
     `,
-    props: ['teams'],    
+    data() {
+        return {
+            teams: this.teamsCollection,
+            houseTeam: null,
+            outTeam: null
+        }
+    },
+    // props: ['teams'],    
+    inject: ['teamsCollection'],
     methods: {
         createNewPlay() {
             let indexHome = Math.floor(Math.random() * 20), 
@@ -265,18 +233,115 @@ Vue.component('new-play', {
                 indexOut = Math.floor(Math.random() * 20); 
             }   
 
-            var houseTeam = this.teams[indexHome];
-            var outTeam = this.teams[indexOut]; 
-            this.$emit('new-play', {houseTeam, outTeam});
+            this.houseTeam = this.teams[indexHome];
+            this.outTeam = this.teams[indexOut]; 
+            this.$refs.modal.showModal();
+            // this.$emit('new-play', {houseTeam, outTeam});
         } 
     }
 })
 
+Vue.component('score-modal', {
+    props: ['houseTeam', 'outTeam'],
+    data() {
+        return {
+            goalsByHouse: 0,
+            goalsByOut: 0
+        }
+    }, 
+    template: `
+        <modal ref="modal">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">New Play</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+             </div>
+            <div class="modal-body">
+                <form action="" class="form-inline">                    
+                    <input type="text" class="form-control col-md-1" v-model="goalsByHouse">                
+                    <dteam :team="houseTeam" inverse="true"></dteam>
+
+                    <span><h2>X</h2></span>
+
+                    <dteam :team="outTeam"></dteam>                
+                    <input type="text" class="form-control col-md-1" v-model="goalsByOut">                
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" @click="endPlay">Fim de Jogo</button>            
+            </div>
+        </modal>
+    `,
+    methods: {
+        endPlay() {
+            var gm = parseInt(this.goalsByHouse); 
+            var gs = parseInt(this.goalsByOut);
+            this.houseTeam.endPlay(this.outTeam, gm, gs);            
+            this.closeModal();
+            // this.$emit('end-play');
+        },
+        showModal() {            
+            this.$refs.modal.show();
+            
+        },
+        closeModal() {
+            this.$refs.modal.close();
+        }        
+    }
+})
+
+
+Vue.component('modal', {        
+    template: `
+    <div class="modal fade" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+    `,   
+    methods: {        
+        show() {
+            $(this.$el).modal('show');
+        },
+        close() {
+            $(this.$el).modal('hide');
+        }
+    }
+})
 
 new Vue({
     el: "#app",
     template: '<my-app></my-app>',
-    
+    provide() {
+        return {
+            teamsCollection: [
+                new Team('América MG', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/america_mg_60x60.png?raw=true'),
+                new Team('Atlético PR', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/atletico-pr_60x60.png?raw=true'),
+                new Team('Atlético MG', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/atletico_mg_60x60.png?raw=true'),
+                new Team('Bahia', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/bahia_60x60.png?raw=true'),
+                new Team('Botafogo', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/botafogo_60x60.png?raw=true'),
+                new Team('Ceará', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/ceara_60x60.png?raw=true'),
+                new Team('Chapecoense', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/chapecoense_60x60.png?raw=true'),
+                new Team('Corinthians', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/corinthians_60x60.png?raw=true'),
+                new Team('Cruzeiro', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/cruzeiro_60x60.png?raw=true'),
+                new Team('Flamengo', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/flamengo_60x60.png?raw=true'),
+                new Team('Fluminense', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/fluminense_60x60.png?raw=true'),
+                new Team('Gremio', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/gremio_60x60.png?raw=true'),
+                new Team('Internacional', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/internacional_60x60.png?raw=true'),
+                new Team('Palmeiras', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/palmeiras_60x60.png?raw=true'),
+                new Team('Paraná', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/parana_60x60.png?raw=true'),
+                new Team('Santos', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/santos_60x60.png?raw=true'),
+                new Team('São Paulo', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/sao_paulo_60x60.png?raw=true'),
+                new Team('Sport', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/sport_60x60.png?raw=true'),
+                new Team('Vasco', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/vasco_60x60.png?raw=true'),
+                new Team('Vitória', 'https://github.com/schoolofnetcom/vue2-rev3/blob/master/assets/vitoria_60x60.png?raw=true')
+            ]
+        }
+    },    
     computed: {
         liberatorsTeams() {
             return this.teams.slice(0,6)
